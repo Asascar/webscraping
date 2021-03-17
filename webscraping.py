2627004ddea15c458e92a250f88ec436820615b7
Asascar <asascar2@gmail.com>
2021-03-16 22:52:38 -0300

    centralização da lógica de scrapping no arquivo principal, deixando apenas as lógicas individuais para seus respectivos arquivos.

--- webscraping.py
+++ webscraping.py
@@ -16,10 +16,19 @@ dicionario = [
         "Banco do Brasil", "Banco do Nordeste", "Banco da Amazônia", "Caixa Econômica Federal", "Banco Central", "Casa da Moeda", "Comissão de Valores Mobiliários", "Susep", "Superintendência de Seguros Privados", "BNDES", "Associação Brasileira de Fundos Garantidores", "BB", "BNB", "BASA", "CEF", "CMB", "ABFG", "AGROS","CAPEF","CAPESESP","CENTRUS","CERES","CIBRIUS","CIFRAO","ELETROS","ELOS","FACHESF","FAPES","FIOPREV","FIPECQ","FUNCEF","FUNPRESP-EXE","FUNPRESP-JUD","FUSESC","GEIPREV","INFRAPREV","NUCLEOS","PETROS","POSTALIS","PREVBEP","PREVDATA","PREVI","PREVINORTE","REAL GRANDEZA","REFER","SAO FRANCISCO","SERPROS","SIAS","CAPAF","PORTUS","TCU"
   ]
 
+
 for item in dicionario:
-  folhascrapping(item,wb,browser)
-  cnnscrapping(item,wb,browser)
-  epocascrapping(item,wb,browser)
-  g1scrapping(item,wb,browser)
-  vejascrapping(item,wb,browser)
-  wb.save("C:/Projetos/PythonSelenium/Materiamidia.xlsx")
+  try:
+    ws = wb[item]
+  except:
+    wb.create_sheet(item)
+    ws = wb[item]
+  linha = ("Titulo","Link","Midia","Data")
+  ws.append(linha)
+  folhascrapping(item,ws,browser)
+  cnnscrapping(item,ws,browser)
+  epocascrapping(item,ws,browser)
+  g1scrapping(item,ws,browser)
+  vejascrapping(item,ws,browser)
+  wb.save("Materiamidia.xlsx")
+print("Finish")
